14d81a46ade413cdca1086fb0104b0e249b7be39
Boris <boriscizman@gmail.com>
2019-02-14 19:19:24 +0100

    nadeljevanje

--- KPO/Math.cs
+++ KPO/Math.cs
@@ -13,13 +13,69 @@ namespace KPO
         public int SumOfN(int n)
         {
             int i;
-            int sum = 10; 
+            int sum = 0; 
             
             for (i = 0; i <= n; i++)
             {
                 sum = sum + i;
             }
+
             return sum;
+        }
+
+        public int SumOfAvgArray(int[] arr)
+        {
+            int sum = 0;
+
+            int average = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            average = sum / arr.Length;
+
+            return average;
+        }
+
+        public List<double> QuadraticEquation(double a, double b, double c)
+        {
+            int m;
+            double r1, r2, d1;
+            d1 = b * b - 4 * a * c;
+            if (a == 0.0)
+                m = 1;
+            else if (d1 > 0.0)
+                m = 2;
+            else if (d1 == 0.0)
+                m = 3;
+            else
+                m = 4;
+            List<double> lista = new List<double>();
+            switch (m)
+            {
+                case 1:
+                    Console.WriteLine(@"\n Not a Quadratic equation, 
+                                          Linear equation");
+                    lista = null;
+                    break;
+                case 2:
+                    lista.Add((-b + System.Math.Sqrt(d1)) / (2 * a));
+                    lista.Add((-b - System.Math.Sqrt(d1)) / (2 * a));
+                    break;
+                case 3:
+                    r1 = r2 = (-b) / (2 * a);
+                    lista.Add((-b + System.Math.Sqrt(d1)) / (2 * a));
+                    lista.Add((-b - System.Math.Sqrt(d1)) / (2 * a));
+                    break;
+                case 4:
+                    Console.WriteLine("\n Roots are Imaginary");
+                    lista.Add((-b) / (2 * a));
+                    lista.Add(System.Math.Sqrt(-d1) / (2 * a));
+                    break;
+            }
+            return lista;
         }
     }
 }
--- UnitTestKPO/UnitTest1.cs
+++ UnitTestKPO/UnitTest1.cs
@@ -1,17 +1,59 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using KPO;
+using System.Collections.Generic;
+
 namespace UnitTestKPO
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestSumOfN()
         {
             KPO.Math ss = new KPO.Math();
             int test = ss.SumOfN(10);
             Assert.AreEqual(test, 55);
+        }
+        [TestMethod]
+        public void TestSumNArray()
+        {
+            KPO.Math ss = new KPO.Math();
+            int[] array = { 10, 10, 10, 10 };
+            int test = ss.SumOfAvgArray(array);
+            Assert.AreEqual(test, 10);
+        }
+        [TestMethod]
+        public void TestDiskriminanta()
+        {
+            KPO.Math ss = new KPO.Math();
+
+            // Linarna enčaba ko je a=0
+            List<double> test = ss.QuadraticEquation(0,1.1,2);
+            Assert.IsNull(test);
+
+            List<double> lista = new List<double>();
+            lista.Add(-0.43844718719116971);
+            lista.Add(-4.5615528128088307);
+
+            // Ko je diskriminanta pozitivna 
+            List<double> test2 = ss.QuadraticEquation(2, 10, 4);
+            CollectionAssert.AreEqual(lista, test2);
+
+            lista = new List<double>();
+            lista.Add(-1);
+            lista.Add(-1);
+            //Ko je diskriminanta 0  
+            List<double> test3 = ss.QuadraticEquation(1, 2, 1);
+            CollectionAssert.AreEqual(lista, test3);
+
+            lista = new List<double>();
+            lista.Add(-0.1);
+            lista.Add(0.7);
+            //Ko je diskriminanta negatvina... imaginarna
+            List<double> test4 = ss.QuadraticEquation(10, 2, 5);
+            CollectionAssert.AreEqual(lista, test4);
+                       
         }
     }
 }
